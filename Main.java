8d235ef5ce1f125789a55e1471998942b3520428
kirtiman-s-r <kirtimansingh61@gmail.com>
2026-04-02 23:38:15 +0530

    feat: add logging variables and print statements

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Sentinel is watching");
+    public static void main(String[] args) {
+        // Variables for log entry
+        String level = "ERROR";
+        String service = "PaymentService";
+        String message = "Payment failed for user 42";
+        int userId = 33;
+        double responseTime = 2.35;
+        boolean isResolved = false;
+
+        // Print them
+        System.out.println(level);
+        System.out.println(service);
+        System.out.println(message);
+        System.out.println(userId);
+        System.out.println(responseTime);
+        System.out.println(isResolved);
+
     }
 }
-
