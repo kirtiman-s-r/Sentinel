d640aa5615d33890d88238cb3e1b4a2e50a5f221
kirtiman-s-r <kirtimansingh61@gmail.com>
2026-04-03 00:44:05 +0530

    refactor: move code to practice pacakge and fix class nesting

--- practice/Main.java
+++ practice/Main.java
@@ -1,3 +1,5 @@
+package practice;
+
 public class Main {
     public static void main(String[] args) {
         // Variables for log entry
---
+++ practice/Member.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Member {
+        public static void main (String[] args){
+            String memberName = "Kirtiman";
+            int memberAge = 19;
+            String targetGrade = "N2";
+            double dailyStudyHours = 3.5;
+            boolean isAccountActive = true;
+
+            System.out.println("practice.Main.Member: " + memberName + ", Age: " + memberAge + ", Target: " + targetGrade + ", Hours: " + dailyStudyHours + ", Active: " + isAccountActive);
+
+        }
+}
+
