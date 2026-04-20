afaef50ad778f9680029674cb0b98e741f1eb48e
kirtiman-s-r <kirtimansingh61@gmail.com>
2026-04-20 23:58:04 +0530

    feat: complete log generator homework levels 1 and 2

--- practice/week3/LogGenerator.java
+++ practice/week3/LogGenerator.java
@@ -6,9 +6,18 @@ public class LogGenerator {
         System.out.println("");
 
         int errorCount = 0;
+        int warnCount = 0;
+        String service = "HI";
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             int logId = i +1;
+
+            if (i < 10) {
+                service = "Payment Service";
+            } else if (i >= 10) {
+                service = "AuthService";
+            }
+
 
             // Every 3rd log is ERROR, every 3rd+1 is WARN, rest INFO
             String level;
@@ -17,16 +26,18 @@ public class LogGenerator {
                 errorCount++;
             } else if (i % 3 == 1) {
                 level = "WARN";
+                warnCount++;
             } else {
                 level = "INFO";
             }
-            System.out.println("Log #" + logId + " [" + level + "] PaymentService | Entry generated");
+            System.out.println("Log #" + logId + " [" + level + "] " + service + " | " + "Entry generated");
         }
 
         System.out.println("");
-        System.out.println("Total logs generated: 10");
+        System.out.println("Total logs generated: 20");
 
         System.out.println("Errors found: " + errorCount);
+        System.out.println("Warning Counts: " + warnCount);
 
     }
 }
