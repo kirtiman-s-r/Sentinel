70da7d9bca422003679154c0e1047bdb118de82a
kirtiman-s-r <kirtimansingh61@gmail.com>
2026-05-06 00:58:13 +0530

    feat: for loops and nested conditions for WeekThreeSummary

---
+++ practice/week3/WeekThreeSummary.java
@@ -0,0 +1,79 @@
+package practice.week3;
+
+public class WeekThreeSummary {
+    static void main(String[] args) {
+
+        System.out.println("=== SENTINEL LOG PROCESSOR ===\n");
+        System.out.println("System initializing...");
+
+        int systemCheck = 0;
+        boolean isInitializationComplete = false;
+
+        do {
+            systemCheck++;
+            if (systemCheck <= 2) {
+                System.out.println("Check #" +systemCheck + " complete.");
+            } else if (systemCheck == 3) {
+                System.out.println("Check #" + systemCheck + " complete. System ready.");
+                isInitializationComplete = true;
+            }
+
+        } while (systemCheck <= 3 && !isInitializationComplete);
+
+        System.out.println("\nProcessing 50 logs...\n");
+        int totalLogsProcessed = 0;
+        int errorCount = 0;
+        int skippedLogs = 0;
+        boolean isLogNumberExceed = false;
+
+        for (int i = 1; i <=50; i++) {
+            totalLogsProcessed++;
+
+            if (i <= 50 && !isLogNumberExceed ) {
+                System.out.println("[" + errorType + "] " + service + " | "+ "Log #" +totalLogsProcessed + " - " + decisionTaken);
+            } else if  ((i <= 50 && !isLogNumberExceed )){
+                System.out.println(errorType + " detected at Log #" +totalLogsProcessed + " - " + decisionTaken);
+            }
+
+            String errorType;
+            if (i % 4 == 0) {
+                errorType = "CRITICAL";
+                isLogNumberExceed = true;
+                break;
+            } else if (i % 3 == 0) {
+                errorType = "ERROR";
+                errorCount++;
+            } else if (i % 5 == 0) {
+                errorType = "INFO";
+                skippedLogs++;
+                continue;
+            }else{
+                errorType = "WARN";
+            }
+
+            String service;
+            if (i % 2 == 0) {
+                service = "PaymentService";
+            } else {
+                service = "AuthService";
+            }
+
+            String decisionTaken = "";
+            if (errorType.equals("ERROR")) {
+                decisionTaken = "ALERT SENT";
+            } else if (errorType.equals("CRITICAL")) {
+                decisionTaken = "halting scan";
+            }
+
+
+        }
+
+        String systemCondition = (errorCount > 0)? "DEGRADED" : "HEALTHY";
+
+        System.out.println("\n=== FINAL REPORT ===");
+        System.out.println("Total processes: " + totalLogsProcessed);
+        System.out.println("Errors found: " + errorCount);
+        System.out.println("INFO skipped: " + skippedLogs);
+        System.out.println("System status: " + systemCondition);
+    }
+}
